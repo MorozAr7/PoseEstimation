2f7f618d6650d04ee2eb37dd268d382adaf98043
Artem Moroz <elodar230101@gmail.com>
2023-08-26 15:54:30 +0200

    Adding path to file

--- DatasetRenderer/RendererConfig.py
+++ DatasetRenderer/RendererConfig.py
@@ -2,6 +2,7 @@ import sys
 
 ROOT_PATH = sys.path[0]
 print(ROOT_PATH)
+print(sys.path[1])
 DATASET_PATH = ROOT_PATH + "/" + "Dataset/"
 OBJECT_TYPE = "Chassis"
 CAM_DATA_FILE = "camera_data_1.json"
