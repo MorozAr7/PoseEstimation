699eef2ea647228ffff4d2e8cd791862170f4446
Artem Moroz <elodar230101@gmail.com>
2023-08-26 16:19:53 +0200

    Adding path to file

--- DatasetRenderer/Renderer.py
+++ DatasetRenderer/Renderer.py
@@ -7,6 +7,7 @@ import cv2
 import sys
 sys.path.insert(0, MAIN_DIR_PATH + '/CameraData')
 sys.path.insert(0, MAIN_DIR_PATH + '/Utils')
+print(MAIN_DIR_PATH + '/Utils')
 from MathUtils import Transformations
 from IOUtils import IOUtils
 o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel(0))
