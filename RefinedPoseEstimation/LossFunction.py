bb6f4fb688436f43c841057e41f05e4de3636ecf
Artem Moroz <elodar230101@gmail.com>
2023-09-13 01:20:04 +0200

    Minor change

--- RefinedPoseEstimation/LossFunction.py
+++ RefinedPoseEstimation/LossFunction.py
@@ -91,9 +91,10 @@ class ProjectionLoss(nn.Module):
         elif self.ProjectionType == "2D":
             transformed_pc_prediction = self.camera_intrinsic @ transformed_pc_prediction
             transformed_pc_target = self.camera_intrinsic @ transformed_pc_target
-            #transformed_pc_prediction[:, 2:3, :][.where(homogenous_coords_2d[2, :] == 0)] = 1
-            xy_projected_predicted = transformed_pc_prediction[:, :2, :] / (transformed_pc_prediction[:, 2:3, :] + 1)
-            xy_projected_target = transformed_pc_target[:, :2, :] / (transformed_pc_target[:, 2:3, :] + 1)
+            transformed_pc_prediction[:, 2, :][torch.where(transformed_pc_prediction[:, 2, :] == 0)] = 1
+            transformed_pc_target[:, 2, :][torch.where(transformed_pc_target[:, 2, :] == 0)] = 1
+            xy_projected_predicted = transformed_pc_prediction[:, :2, :] / (transformed_pc_prediction[:, 2, :])
+            xy_projected_target = transformed_pc_target[:, :2, :] / (transformed_pc_target[:, 2, :])
             if vis:
                 self.visualize_projection(xy_projected_predicted, xy_projected_target)
             return self.L1_loss(xy_projected_predicted, xy_projected_target)
