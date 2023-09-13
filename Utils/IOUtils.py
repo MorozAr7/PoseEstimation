046648903a6c4b05e1c05ed5e67ca4ec037ebfc3
Artem Moroz <elodar230101@gmail.com>
2023-09-13 09:14:27 +0200

    Minor change

--- Utils/IOUtils.py
+++ Utils/IOUtils.py
@@ -10,21 +10,21 @@ class IOUtils:
 	def load_json_file(path: str) -> dict:
 		with open(path, "r") as f:
 			data_dict = json.load(f)
-			f.close()
+		f.close()
 		return data_dict
 
 	@staticmethod
 	def load_numpy_file(path: str) -> np.array:
 		with open(path, "rb") as f:
 			data_array = np.load(f)
-			f.close()
+		f.close()
 		return data_array
 
 	@staticmethod
 	def save_json_file(path: str, data_dict: dict) -> None:
 		with open(path, "w") as f:
 			json.dump(data_dict, f)
-			f.close()
+		f.close()
 
 	@staticmethod
 	def save_numpy_file(path: str, data_array: np.array) -> None:
